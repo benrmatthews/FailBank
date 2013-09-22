5813f3bf12c978c1e42531ffb525f1e3b0d9944e
benrmatthews <ben@brightone.org.uk>
2013-09-22 19:54:42 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Failbank::Application.config.secret_token = '113976911006d53b93233be3aeef749f79a78f1f9c7bd63aa5af4bc47730907989f60f4ff5b8b026fb6cb1f5d019008e9377e0ea4288dd3df9775d536fe3d4ef'
