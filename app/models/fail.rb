b56f88f8dc5c8a72b68c7f7c5deec32f8cec8eb5
benrmatthews <ben@brightone.org.uk>
2013-09-27 14:54:18 +0000

    Added comments model

--- app/models/fail.rb
+++ app/models/fail.rb
@@ -1,10 +1,12 @@
 class Fail < ActiveRecord::Base
   attr_accessible :description, :lesson, :name, :user_id
-  has_many :comments, as: :commentable
-  
+    
   belongs_to :user
   
   validates :user_id, presence: true
   
   default_scope order: 'fails.created_at DESC'
+  
+  has_many :comments, as: :commentable
+
 end
